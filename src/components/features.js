426258554bc4d0dea9979dc129f2778e2de4ea55
nikita-seliverstov <fubirino@gmail.com>
2019-07-22 13:24:04 +0300

    code style

--- src/components/features.js
+++ src/components/features.js
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types'
 //import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="container">
-  <div className="columns">
-    <div className="row">
-    {gridItems.map(item => (
-      <div key={item.blockHeading} className="col-6">
-      <h1>{item.blockHeading}</h1>
-         <p><em><strong>
-         {item.blockDescription}
-         </strong></em></p>
-         <button type="button" class="btn btn-primary"><strong>{item.buttonPlaceholder}</strong></button>
+  
+    gridItems.map(item => (
+      <div className="container">
+        <div className="row">
+          <div key={item.blockHeading} className="col-6">
+            <h1>{item.blockHeading}</h1>
+            <p><em><strong>
+              {item.blockDescription}
+            </strong></em></p>
+            <button type="button" class="btn btn-primary"><strong>{item.buttonPlaceholder}</strong></button>
+          </div>
+        </div>
       </div>
-    ))}
-     </div>
-          </div>
-          </div>
+    ))
+     
+         
         
 )
 
