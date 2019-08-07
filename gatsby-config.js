0cee6274d703dcfe9911c9520c7e9d61bfb39fa0
nikita <nikita.seliverstov@tptlive.ee>
2019-08-07 11:14:18 +0300

    purgeCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,15 @@ module.exports = {
         useLangKeyLayout: false
       }
     },
+    resolve: `gatsby-plugin-purgecss`,
+    options: {
+      printRejected: true, // Print removed selectors and processed file names
+      // develop: true, // Enable while using `gatsby develop`
+      // tailwind: true, // Enable tailwindcss support
+      // whitelist: ['whitelist'], // Don't remove this selector
+      // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+      // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    },
     'gatsby-plugin-optimize-svgs',
      `gatsby-plugin-netlify`
   ],
