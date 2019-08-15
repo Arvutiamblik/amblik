73181c06e0afcd42f1ccb82b01eddae8217af948
nikita <nikita.seliverstov@tptlive.ee>
2019-08-15 12:45:54 +0300

    test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,18 @@
+
 import CMS from 'netlify-cms-app'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+CMS.init({
+    
+ 
+    collections: [
+     { label: "articleAAA", name: "articleaaaa", folder: "src/content/menu", create: true, fields: [
+     { label: "slug", name: "slug", widget: "string"},
+     { label: "language code", name: "lang", widget: "string"},
+     { label: "Text", name: "text", widget: "string"},
+    ]},
+    ],
+    
+     
+})
 CMS.registerPreviewStyle('../components/layout.css')
 CMS.registerPreviewTemplate('main_page_et', IndexPagePreview)
