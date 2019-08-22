eaf6621dfbb982df968f09fa67b81ed0143c4af0
nikita <nikita.seliverstov@tptlive.ee>
2019-08-22 11:51:33 +0300

    test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,5 +2,74 @@ import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import CMS from 'netlify-cms-app'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import { createHttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import { ApolloClient } from 'apollo-client';
+import { useQuery } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 CMS.registerPreviewStyle('../components/layout.css')
 CMS.registerPreviewTemplate('main_page_et', IndexPagePreview)
+
+
+const client = new ApolloClient({
+    link: createHttpLink({uri: "http://localhost:8000/___graphql", credentials: 'include'}),
+    cache: new InMemoryCache({
+            addTypename: false
+    })
+});
+class dataService {
+    async fetchData(){
+        const result = await client.query({
+            query: gql`query FETCH_DATA{
+            allSitePage {
+                nodes {
+                  path
+                }
+            }
+        }`})
+        CMS.init({
+            config: {
+              backend: {
+                name: 'github',
+                repo: 'Arvutiamblik/amblik',
+                branch: 'master'
+              },
+              load_config_file: false,
+              media_folder: "static/images",
+              public_folder: "/images",
+              collections: [
+                {
+                  label: "menu",
+                  name: "menu",
+                  folder: "src/content/menu",
+                  create: true,
+                  fields: [
+                    { label: "slug", name: "slug", widget: "string" },
+                    { label: "language code", name: "lang", widget: "string" },
+                    {
+                      label: result.data.allSitePage.nodes[0].path,
+                      name: "test",
+                      widget: "string"
+                    }
+                  ]
+                }
+              ]
+            }
+          })
+     
+    }
+              
+                
+       
+        
+}
+   
+     
+
+
+var obj = new dataService();
+console.log(obj.fetchData())
+
+
+
+  
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,65 +4,17 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-import React, { useState, useEffect } from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import CMS from 'netlify-cms-app'
-
+import React from "react"
 import PropTypes from "prop-types"
 import "./layout.css"
-import App from "../cms/cms.js"
-function Layout ({ children }) {
-  const data = useStaticQuery(graphql`
-    query {
-      allSitePage {
-        nodes {
-          path
-        }
-      }
-    }
-  `)
 
-  useEffect(() => {
-    CMS.init({
-      config: {
-      
-          backend: {
-            name: 'github',
-            repo: 'Arvutiamblik/amblik',
-            branch: 'master'
-          },
-          load_config_file: false,
-          media_folder: "static/images",
-          public_folder: "/images",
-        collections: [
-          {
-            label: 'menu',
-            name: 'menu',
-            folder: 'src/content/menu',
-            create: true,
-            fields: [
-              { label: 'slug', name: 'slug', widget: 'string' },
-              { label: 'language code', name: 'lang', widget: 'string' },
-              { label: 'Text', name: 'text', widget: 'string' },
-              {
-                label: data.allSitePage.nodes[0].path,
-                name: 'test',
-                widget: 'string'
-              }
-            ]
-          }
-        ]
-      }
-    })
-  }, [data])
+const Layout = ({ children }) => {
 
   return (
 
-    <main>{children}</main>
-)
+        <main>{children}</main>
+  )
 }
-
-
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
