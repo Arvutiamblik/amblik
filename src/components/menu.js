e37194f61e1b2ac9c4e5aa0b1b4ec5b4297101e1
Foxman322 <43431500+Foxman322@users.noreply.github.com>
2019-11-08 15:17:32 +0200

    fix menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,14 +4,14 @@ import {Link} from "gatsby"
 const menu = () => {
 return (
     <header>
-    <div className="container">
-              <h1 id="logo"><big><strong>amblik </strong></big></h1>
-             <Link to="//"><strong>Home</strong></Link>
+    <div id="headerPosition" className="container">
+             <h1 id="logo"><big><strong>amblik </strong></big></h1>
+             <Link id="link_button" to="/support/"><strong>Support</strong></Link>
                <a href="#" id="link_button"><strong>SHOP</strong></a>
                <a href="#" id="link_button"><strong>SHOP</strong></a>
                <a href="#" id="link_button"><strong>SHOP</strong></a>
                <a href="#" id="language"><strong>eng</strong></a>
-               <button id="support" type="button" classname="btn btn-primary"><Link to="/support/"><strong>Support</strong></Link></button>             
+               <button id="support" type="button" classname="btn btn-primary"><strong>SUPPORT</strong></button>             
         </div>
         </header>
 )
