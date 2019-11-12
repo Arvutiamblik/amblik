caaedeef78cc546397a74a6c8168aaf1c4cb724b
nikita <nikita.seliverstov@tptlive.ee>
2019-11-12 14:10:54 +0200

    fix

--- src/components/productsIntro.js
+++ src/components/productsIntro.js
@@ -19,7 +19,7 @@ return(
 )};
 const Intro = ({ item }) => (
   <>
-  <div style={{height: 'auto', width: '250px', float: 'left', }}>
+  <div style={{height: 'auto', width: '250px'}}>
    <Img fluid={item.product_imageSharp.childImageSharp.fluid} />
    </div>
     <p className="productText" style={{ marginTop: '20px'}}>
