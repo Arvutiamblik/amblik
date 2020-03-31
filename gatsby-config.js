f639db912fbc22492092471ef941ff39fd13d4ea
nikita-seliverstov <fubirino@gmail.com>
2020-03-31 12:37:48 +0300

    rebuild

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
+      
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
