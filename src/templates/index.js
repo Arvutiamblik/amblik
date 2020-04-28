e149f4551089ae84e7ffee3a27d127984632cf24
nikita-seliverstov <fubirino@gmail.com>
2020-04-28 14:11:00 +0300

    naming

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "../components/layout";
-import TextBlock from "../components/textBlock";
+import Layout from "../components/Layout";
+import TextBlock from "../components/TextBlock";
 import { graphql } from "gatsby";
 
 
