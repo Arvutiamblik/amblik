69fee551da581b01f5498499e1b86c5a157aa51f
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-05-14 12:47:57 +0300

    added form fields

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,11 +2,20 @@ import React from "react";
 import { RichText } from "prismic-reactjs";
 
 const ContactForm = (props) => {
+  const formFields = props.formData.form_input.filter(item => item.type !== 'submit')
   const submit = props.formData.form_input.find(item => item.type === 'submit')
+  let counter = 1;
+  console.log(formFields)
   return (
     <form>
       <RichText render={props.formData.form_name}>{props.formData.form_name}</RichText>
-      <button type={submit.type} className="btn btn-primary button-main">{submit.name}</button>
+      {formFields.map((item, index) =>
+        <div key={index} className="form-group">
+          <label htmlFor={`input-${counter}`} className="form-label">{item.name}</label>
+          <input id={`input-${counter++}`} className="form-control form-field" type={item.type} />
+        </div>
+      )}
+      <button type={submit.type} className="btn btn-primary button-main my-3">{submit.name}</button>
     </form>
   );
 };
