8be0f8e980e2715c73083979d3c6b66510fce70c
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-05-15 10:21:37 +0300

    removed form name

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { RichText } from "prismic-reactjs";
 
 const ContactForm = (props) => {
   const formFields = props.formData.form_input.filter(item => item.type !== 'submit')
   const submit = props.formData.form_input.find(item => item.type === 'submit')
   let counter = 1;
-  console.log(formFields)
   return (
     <form>
-      <RichText render={props.formData.form_name}>{props.formData.form_name}</RichText>
       {formFields.map((item, index) =>
         <div key={index} className="form-group">
           <label htmlFor={`input-${counter}`} className="form-label">{item.name}</label>
