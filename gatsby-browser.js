fa5d68b90e57368bb62cea2ad7ac6ec34a65febd
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-06-04 17:41:13 +0300

    fixed anchors after refreshing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,21 @@ const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
 const { linkResolver } = require('./src/linkResolver');
 
 registerLinkResolver(linkResolver);
+
+exports.onRouteUpdate = ({ location }) => scrollToAnchor(location)
+
+/**
+ *
+ * @desc - a function to jump to the correct scroll position
+ * @param {Location} location -
+ */
+function scrollToAnchor(location) {
+  // Check for location so build does not fail
+  if (location && location.hash) {
+    const item = document.querySelector(`${location.hash}`)
+    
+    requestAnimationFrame(() => item.scrollIntoView({behavior: "smooth"}));
+  }
+
+  return true
+}
