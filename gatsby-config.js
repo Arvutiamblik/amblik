3b3872880b425d0611106da4ade53f45eb44fafe
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-07-17 13:29:47 +0300

    added favicon, changed theme color in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#00008b`,
+        theme_color: `#00008b`,
         display: `minimal-ui`,
-       // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/amblik-favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
