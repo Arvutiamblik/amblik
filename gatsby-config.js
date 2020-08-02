370230ae9d46f01a98c650f8efe635fe7c9cf8a9
nikita-seliverstov <37171096+nikita-seliverstov@users.noreply.github.com>
2020-08-02 16:23:36 +0300

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,19 +24,13 @@ module.exports = {
         trackingId: "UA-174254226-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
         // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
   
         defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+     
       
       },
     },
