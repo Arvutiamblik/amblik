ccf7e5929c7fcc8aa47927dbea488f287b8d19f2
nikita-seliverstov <37171096+nikita-seliverstov@users.noreply.github.com>
2020-08-05 08:44:52 +0300

    banner

--- src/templates/article.js
+++ src/templates/article.js
@@ -71,7 +71,7 @@ if (!test) return null
         text={article.node.text}
         feedbackForm={article.node.feedback_form}
         articleSlices={data.prismic.article}
-        articleBanner={  article.node.banner !== null  ? article.node.banner : null}
+        articleBanner={ article.node.banner.url}
       />
     </Layout>
   );
