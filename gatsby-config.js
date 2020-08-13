5975ea4e5cfd38993b77b91a09a34728b961e317
nikita-seliverstov <37171096+nikita-seliverstov@users.noreply.github.com>
2020-08-13 14:00:45 +0300

    fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
    
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Montserrat`,
-          `100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i`,
-        ],
-        display: "optional",
-      },
-    },
+  
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
