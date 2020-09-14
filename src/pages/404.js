01400477d7a19d72bd4842e3d3ec2c9553889c18
feloriel <33422762+feloriel@users.noreply.github.com>
2020-09-14 13:02:11 +0300

    added 404 redirect to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
-import React from "react"
-import SEO from "../components/seo"
+import React from "react";
+import SEO from "../components/seo";
+import { useEffect } from "react";
+import { navigate } from "gatsby";
 
-
-const NotFoundPage = ({pageContext, location }) => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-</>
-)
-
-export default NotFoundPage
+export default () => {
+  useEffect(() => {
+    navigate("/");
+  }, []);
+  return null;
+};
