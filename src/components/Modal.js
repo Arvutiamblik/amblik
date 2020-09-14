3e58cdcb350bd1eb41f5e66c94a0bf2e5280b5a4
feloriel <33422762+feloriel@users.noreply.github.com>
2020-09-14 17:56:57 +0300

    replaced callto with tel in phone links

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import TeamviewerBtnImg from '../images/teamviewer-button.png';
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import TeamviewerBtnImg from "../images/teamviewer-button.png";
 
 const ModalWindow = (props) => {
   const { supportModal } = props;
@@ -9,29 +9,53 @@ const ModalWindow = (props) => {
 
   return (
     <React.Fragment>
-      <Button className="button-main button_support text-uppercase" onClick={toggle}>{supportModal.modal_button_text}</Button>
+      <Button
+        className="button-main button_support text-uppercase"
+        onClick={toggle}
+      >
+        {supportModal.modal_button_text}
+      </Button>
       <Modal isOpen={modal} toggle={toggle} className="support-modal">
         <ModalHeader toggle={toggle}>{supportModal.title}</ModalHeader>
         <ModalBody>
           <div className="d-flex justify-content-center mb-3">
-            <div>{supportModal.phone_title}:
+            <div>
+              {supportModal.phone_title}:<span>&nbsp;</span>
+              <a href={`tel:${supportModal.phone_number.replace(/\s/g, "")}`}>
+                {supportModal.phone_number}
+              </a>
               <span>&nbsp;</span>
-              <a href={`callto: ${supportModal.phone_number}`}>{supportModal.phone_number}</a>
-              <span>&nbsp;</span>
-              <a href={`callto: ${supportModal.mobile_phone_number}`}>{supportModal.mobile_phone_number}</a>
+              <a
+                href={`tel:${supportModal.mobile_phone_number.replace(
+                  /\s/g,
+                  ""
+                )}`}
+              >
+                {supportModal.mobile_phone_number}
+              </a>
             </div>
           </div>
           <div className="d-flex justify-content-center">
-            <div>{supportModal.email_title}:
-              <span>&nbsp;</span>
-              <a href={`mailto: ${supportModal.email}`}> {supportModal.email}</a>
+            <div>
+              {supportModal.email_title}:<span>&nbsp;</span>
+              <a href={`mailto: ${supportModal.email}`}>
+                {" "}
+                {supportModal.email}
+              </a>
             </div>
           </div>
         </ModalBody>
         <ModalFooter>
-          <div><h6>{supportModal.subtitle}</h6></div>
+          <div>
+            <h6>{supportModal.subtitle}</h6>
+          </div>
           <div className="download-button">
-            <a href={supportModal.teamviewer_support_link.url} target="_blank" rel="noopener noreferrer" alt={supportModal.button_text}>
+            <a
+              href={supportModal.teamviewer_support_link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              alt={supportModal.button_text}
+            >
               <img src={TeamviewerBtnImg} alt={supportModal.button_text} />
               <span>{supportModal.button_text}</span>
             </a>
@@ -41,6 +65,6 @@ const ModalWindow = (props) => {
       </Modal>
     </React.Fragment>
   );
-}
+};
 
 export default ModalWindow;
