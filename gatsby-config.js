8fcfc3d5232e4eb1ae7586cbab098ccd9e152e0d
feloriel <33422762+feloriel@users.noreply.github.com>
2020-09-15 16:21:15 +0300

    added maskable icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,11 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-119752018-3",
+        trackingId: `UA-119752018-3`,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        exclude: [`/preview/**`, `/do-not-track/me/too/`],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
 
@@ -23,48 +23,76 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: `gatsby-plugin-google-tagmanager`,
       options: {
-        id: "GTM-P27DLKQ",
+        id: `GTM-P27DLKQ`,
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Amblik`,
         start_url: `/`,
         background_color: `#00008b`,
         theme_color: `#00008b`,
         display: `minimal-ui`,
-        icon: `src/images/amblik-favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/amblik-favicon.png`,
+        icons: [
+          {
+            src: `icons/icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-256x256.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: `maskable`,
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
     {
-      resolve: "gatsby-source-prismic-graphql",
+      resolve: `gatsby-source-prismic-graphql`,
       options: {
-        repositoryName: "amblik", // (REQUIRED, replace with your own)
-        path: "/preview",
+        repositoryName: `amblik`, // (REQUIRED, replace with your own)
+        path: `/preview`,
         previews: true,
         omitPrismicScript: true,
         shortenUrlLangs: true,
-        defaultLang: "et-et",
-        langs: ["et-et", "ru", "en-us"],
+        defaultLang: `et-et`,
+        langs: [`et-et`, `ru`, `en-us`],
         pages: [
           {
-            type: "Home_page",
-            match: "/:lang?/",
-            path: "/home-prewview",
-            component: require.resolve("./src/templates/index.js"),
+            type: `Home_page`,
+            match: `/:lang?/`,
+            path: `/home-prewview`,
+            component: require.resolve(`./src/templates/index.js`),
           },
           {
-            type: "Article",
-            match: "/:lang?/:uid",
-            path: "/article",
-            component: require.resolve("./src/templates/article.js"),
+            type: `Article`,
+            match: `/:lang?/:uid`,
+            path: `/article`,
+            component: require.resolve(`./src/templates/article.js`),
           },
         ],
       },
@@ -84,23 +112,23 @@ module.exports = {
       options: {
         policy: [
           {
-            userAgent: "Googlebot",
-            allow: "/",
-            disallow: ["/preview", "/home-prewview"],
+            userAgent: `Googlebot`,
+            allow: `/`,
+            disallow: [`/preview`, `/home-prewview`],
             crawlDelay: 2,
           },
           {
-            userAgent: "OtherBot",
-            allow: ["/allow-for-all-bots", "/allow-only-for-other-bot"],
-            disallow: ["/preview", "/home-prewview"],
+            userAgent: `OtherBot`,
+            allow: [`/allow-for-all-bots`, `/allow-only-for-other-bot`],
+            disallow: [`/preview`, `/home-prewview`],
             crawlDelay: 2,
           },
           {
-            userAgent: "*",
-            allow: "/",
-            disallow: ["/preview", "/home-prewview"],
+            userAgent: `*`,
+            allow: `/`,
+            disallow: [`/preview`, `/home-prewview`],
             crawlDelay: 10,
-            cleanParam: "ref /articles/",
+            cleanParam: `ref /articles/`,
           },
         ],
       },
